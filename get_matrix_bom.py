88290e884fc9fdf0ce8ce793f9e524d8b922e7f5
qfu <qianghua.fu@gmail.com>
2018-11-05 11:43:54 +1300

    Works with one item, i.e. a0.

--- get_matrix_bom.py
+++ get_matrix_bom.py
@@ -1,30 +1,32 @@
 import pandas as pd 
 import os 
 
+#This function returns a full filename i.e. with extension csv.
 def csvize(filename):
     return filename + '.csv'
 
-parent = 'a0'
-df_parent = pd.read_csv(csvize(parent))
+#It
+def expand(c, p):
+    df = pd.read_csv(csvize(c))
+    df['quantity'] *= float(p[p['child'] == c]['quantity'])
+    return df
 
 matrix_bom = pd.DataFrame(columns = ['child', 'quantity', 'level'])
 
-level = 0
-while not df_parent.empty:
-    matrix_bom = matrix_bom.append(df_parent)
-    matrix_bom.level = level
-    print(matrix_bom)
-    df_child = pd.DataFrame(columns = ['child', 'quantity'])
-    
-    for i in df_parent.child:
-        if csvize(i) in os.listdir(): 
-            df = pd.read_csv(csvize(i)) 
-            df.quantity *= float(df_parent[df_parent.child == i].quantity)
-            df_child = df_child.append(df)
-            print(i)
-            print(df_child)
-            print()
+level = 1
+parent = pd.read_csv(csvize('a0'))
+parent['level'] = level
+
+while not parent.empty:
+
     level += 1
-    matrix_bom.append(df_child)
-    df_parent = df_child
-print(matrix_bom)
+
+    matrix_bom =pd.concat([matrix_bom, parent])
+    child = [df(i, parent) for i in parent['child'] if csvize(i) in os.listdir()]
+
+    if child:
+        child = pd.concat(child, ignore_index = True)
+        child['level'] = level
+        parent = child
+    else:
+        break
