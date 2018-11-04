5fc8011a31d2ae15558156b483985ba9713ffaed
rick <qianghua.fu+git@gmail.com>
2018-11-04 20:58:51 +1300

    more bugs

--- get_matrix_bom.py
+++ get_matrix_bom.py
@@ -7,16 +7,24 @@ def csvize(filename):
 parent = 'a0'
 df_parent = pd.read_csv(csvize(parent))
 
-matrix_bom = pd.DataFrame(columns = ['child', 'quantity'])
+matrix_bom = pd.DataFrame(columns = ['child', 'quantity', 'level'])
+
+level = 0
 while not df_parent.empty:
     matrix_bom = matrix_bom.append(df_parent)
+    matrix_bom.level = level
+    print(matrix_bom)
     df_child = pd.DataFrame(columns = ['child', 'quantity'])
     
     for i in df_parent.child:
-        if i + '.csv' in os.listdir(): 
-            df = pd.read_csv(i + '.csv') 
+        if csvize(i) in os.listdir(): 
+            df = pd.read_csv(csvize(i)) 
             df.quantity *= float(df_parent[df_parent.child == i].quantity)
             df_child = df_child.append(df)
-    matrix_bom = matrix_bom.append(df_child)
+            print(i)
+            print(df_child)
+            print()
+    level += 1
+    matrix_bom.append(df_child)
     df_parent = df_child
 print(matrix_bom)
